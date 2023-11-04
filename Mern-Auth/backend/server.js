f6ae4425e0b2edbcfb2fc7314ce61fef7e7357bf
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-11-05 04:50:44 +0530

    update

--- Mern-Auth/backend/server.js
+++ Mern-Auth/backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
-const port = 5000;
-const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express(); 
 
 app.get('/', (req, res) => res.send('Server is ready to serve'));
 app.listen(port, ()=>console.log(`Server is listening on port:${port}`));
