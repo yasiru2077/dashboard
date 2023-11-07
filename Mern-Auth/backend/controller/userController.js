3effcd368bbcdb7654facb927c52ca3288372773
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-11-07 21:18:59 +0530

    update

--- Mern-Auth/backend/controller/userController.js
+++ Mern-Auth/backend/controller/userController.js
@@ -1,8 +1,10 @@
 // @description Auth user/set token
 // route POST /api/users/auth
 // @access Public
-const authUser = (req, res) => {
+const authUser =async (req, res) => {
     res.status(200).json({message:'Auth User'});
 };
+
+
 
 export {authUser};
