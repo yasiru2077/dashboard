d71b0ff49bf31652d983dd6d1a08d83f82b86352
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-11-14 00:12:54 +0530

    update

--- Mern-Auth/backend/controller/userController.js
+++ Mern-Auth/backend/controller/userController.js
@@ -3,9 +3,21 @@ import asyncHandler from "express-async-handler";
 // route POST /api/users/auth
 // @access Public
 const authUser =asyncHandler (async (req, res) => {
+
     res.status(200).json({message:'Auth User'});
+
 });
 
+const registerUser = asyncHandler (async (req, res) => {
 
+    res.status(200).json({message:'Register User'});
+
+});
+
+const LogoutUser = asyncHandler (async (req, res) => {
+
+    res.status(200).json({message:'Logout User'});
+
+});
 
 export { authUser };
