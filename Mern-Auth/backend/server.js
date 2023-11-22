826e75e1938e72dbe5d042e48c1d40f3d8002f19
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-11-22 21:12:56 +0530

    update

--- Mern-Auth/backend/server.js
+++ Mern-Auth/backend/server.js
@@ -9,7 +9,8 @@ import userRoutes from './routes/userRoutes.js';
 connectDB();
 
 const app = express(); 
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/api/users' ,userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready to serve'));
