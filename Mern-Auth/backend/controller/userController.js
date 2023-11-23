4eb058b7d2be02433134be9fdbaf3f9b25434b32
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-11-23 06:25:29 +0530

    update

--- Mern-Auth/backend/controller/userController.js
+++ Mern-Auth/backend/controller/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 // @description Auth user/set token
 // route POST /api/users/auth
