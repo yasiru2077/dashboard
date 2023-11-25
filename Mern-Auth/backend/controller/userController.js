e8bb9bac97ba538079f0ceb9ca2a09ccd7121a6f
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-11-26 04:28:12 +0530

    update

--- Mern-Auth/backend/controller/userController.js
+++ Mern-Auth/backend/controller/userController.js
@@ -9,7 +9,8 @@ const authUser =asyncHandler (async (req, res) => {
 
     const {email,password}=req.body;
     const user = await User.findOne({email});
-    if(user && (await user.marchPassword(password)){
+
+    if(user && (await user.matchPassword(password))){
         generateToken(res, user._id);
         res.status(201).json({
             _id:user._id,
