0ed63962a45185b2437f14d8804a1dba16bab6fc
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-12-02 02:27:09 +0530

    update

--- Mern-Auth/backend/controller/userController.js
+++ Mern-Auth/backend/controller/userController.js
@@ -54,7 +54,11 @@ const registerUser = asyncHandler (async (req, res) => {
 
 const logoutUser = asyncHandler (async (req, res) => {
 
-    res.status(200).json({message:'Logout User'});
+    res.cookie('jwt', '',{
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({message:'User Logged Out'});
 
 });
 
