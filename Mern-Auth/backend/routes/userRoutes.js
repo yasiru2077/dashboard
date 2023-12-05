23bc785c09d98ae53881f98973968535d81a53d4
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-12-05 23:31:14 +0530

    update

--- Mern-Auth/backend/routes/userRoutes.js
+++ Mern-Auth/backend/routes/userRoutes.js
@@ -5,12 +5,13 @@ import { authUser,
     logoutUser, 
     getUserProfile, 
     updateUserProfile } from '../controller/userController.js';
+import { protect } from '../middleware/authMiddleware.js';    
 import { update } from 'tar';
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put (updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect,updateUserProfile);
 
 router.put('/profile', updateUserProfile);
 
