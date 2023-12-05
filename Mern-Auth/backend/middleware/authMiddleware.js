abf1fe8dd98ed7144b4bb4d917d3c181140897f5
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-12-06 00:12:02 +0530

    update

--- Mern-Auth/backend/middleware/authMiddleware.js
+++ Mern-Auth/backend/middleware/authMiddleware.js
@@ -13,6 +13,7 @@ const decoded = jwt.verify(token,process.env.JWT_SECRET);
 req.user=await User.findById(decoded.userId).select('-password');
 next();
 }catch(error){
+    
     res.status(401);
     throw new Error('Not authorized, invalid token');
 }
--- Mern-Auth/backend/routes/userRoutes.js
+++ Mern-Auth/backend/routes/userRoutes.js
@@ -1,19 +1,19 @@
 import express from 'express';
-const router = express.Router();
 import { authUser, 
     registerUser, 
     logoutUser, 
     getUserProfile, 
     updateUserProfile } from '../controller/userController.js';
 import { protect } from '../middleware/authMiddleware.js';    
-import { update } from 'tar';
+const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(protect, getUserProfile).put(protect,updateUserProfile);
-
-router.put('/profile', updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 
 export default router;
