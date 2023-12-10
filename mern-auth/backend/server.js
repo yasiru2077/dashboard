96247cc7536e1b14b61de031be794ce95641277d
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-12-11 01:47:51 +0530

    update

--- mern-auth/backend/server.js
+++ mern-auth/backend/server.js
@@ -1,1 +1,8 @@
-import express form 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.get('/', (req, res) => res.send('Server is ready'));
+app.listen(port,()=>console.log(`Server stared on port ${port}`));
+
